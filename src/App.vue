e25af838de61740983db0698f8a9338e633177d3
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-05-01 12:42:28 +0900

    マークアップ

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div classs="main">
+  <div class="main">
     <Calendar></Calendar>
   </div>
 </template>
@@ -12,3 +12,22 @@ export default {
   }
 }
 </script>
+<style lang='scss' scope>
+  .main {
+    width: auto;
+    height: 1200px;
+    background-color: rgba(208, 207, 207, 0.4);
+    position: relative;
+  }
+  p {
+    font-family: 'ヒラギノ丸ゴ ProN','Hiragino Maru Gothic ProN',sans-serif;
+    font-size: 2.0rem;
+  }
+  th, td {
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 1.7rem;
+  }
+  tr {
+    vertical-align: middle;
+  }
+</style>
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,6 +1,71 @@
 <template>
-  <div class='calendar'>
-    {{ msg }}
+  <div class="calendar">
+    <div class="calendar-header">
+      <p>May 2019</p>
+    </div>
+    <table class="calendar-main">
+      <thead class='calendar-head-row'>
+        <tr align='center'>
+          <th>S</th>
+          <th>M</th>
+          <th>T</th>
+          <th>W</th>
+          <th>T</th>
+          <th>F</th>
+          <th>S</th>
+        </tr>
+      </thead>
+      <tbody class='calendar-body'>
+        <tr align='center'>
+          <td>26</td>
+          <td>27</td>
+          <td>28</td>
+          <td>29</td>
+          <td>30</td>
+          <td>1</td>
+          <td>2</td>
+        </tr>
+        <tr align='center'>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+        </tr>
+        <tr align='center'>
+          <td>10</td>
+          <td>11</td>
+          <td>12</td>
+          <td>13</td>
+          <td>14</td>
+          <td>15</td>
+          <td>16</td>
+        </tr>
+        <tr align='center'>
+          <td>17</td>
+          <td>18</td>
+          <td>19</td>
+          <td>20</td>
+          <td>21</td>
+          <td>22</td>
+          <td>23</td>
+        </tr>
+        <tr align='center'>
+          <td>24</td>
+          <td>25</td>
+          <td>26</td>
+          <td>27</td>
+          <td>28</td>
+          <td>29</td>
+          <td>30</td>
+        </tr>
+        <tr align='center'>
+          <td>31</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
@@ -14,6 +79,30 @@ export default {
 </script>
 <style lang='scss' scope>
   .calendar {
-    color: red;
+    width: 50%;
+    height: 50%;
+    box-sizing: border-box;
+    margin: 0 auto;
+    background-color: white;
+    box-shadow: 1px 1px 1px 1px rgba(211, 206, 206, 0.4);
+    position: relative;
+    top: 10%;
+    border-radius: 1px;
+  }
+
+  .calendar-header p {
+    text-align: center;
+    padding-top: 10%;
+  }
+
+  .calendar-main {
+    width: 100%;
+    height: 70%;
+    margin: 5% auto auto auto;
+    position: relative;
+  }
+
+  .calendar-head-row th {
+    opacity: 0.2;
   }
 </style>
