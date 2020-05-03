d2961711154eeafa0811cdb3ad14d10334ee0d3d
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-05-03 20:53:57 +0900

    モーダルコンポーネントの追加

---
+++ src/components/CalendarModal.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="calendar-modal">
+    <div class="wrapper">
+      <div class="modal-header">
+      </div>
+      <div class="modal-main">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang='scss' scoped>
+  .calendar-modal {
+    width: 300px;
+    height: 300px;
+  }
+</style>
